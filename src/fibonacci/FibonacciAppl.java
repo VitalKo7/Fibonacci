d2c6314f92ec87371d3ae9a87a8d2299e8d15fe9
Vitaliy Kornienko <vit_ko@ukr.net>
2023-11-07 18:00:10 +0100

    array solution

--- src/fibonacci/FibonacciAppl.java
+++ src/fibonacci/FibonacciAppl.java
@@ -1,9 +1,26 @@
 package fibonacci;
 
+import java.util.Arrays;
+
 public class FibonacciAppl {
     public static void main(String[] args) {
         // todo: print all numbers (8 items) and calculate the sum;
+        int[] fibonacci = new int[8];
+        int count = 0;
+        fibonacci[count] = 1; // count = 0
+        fibonacci[count + 1] = 1; // count = 1
 
+        int sum = 0;
+        sum = fibonacci[count] + fibonacci[count + 1];
+
+        for (count = 2; count < fibonacci.length; count++) {
+            fibonacci[count] = fibonacci[count - 2] + fibonacci[count - 1];
+            sum += fibonacci[count];
+        }
+
+        System.out.println(Arrays.toString(fibonacci));
+        System.out.println(fibonacci.length);
+        System.out.println(sum);
     }
 }
 
